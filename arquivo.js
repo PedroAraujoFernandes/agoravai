dbf9139729034637998c34c6360e63eacccb6702
pedro <solua@gmail.com>
2026-05-07 17:36:02 -0300

    não compativel com numeros abaixo de 0

--- arquivo.js
+++ arquivo.js
@@ -4,20 +4,37 @@ let resultado = document.getElementById("resultado");
 
 botao.onclick = function(){
     
+    
+    
     let altura = document.getElementById("altura").value;
     let peso = document.getElementById("peso").value;
-    
     let resposta;
     
-    resposta = peso / (altura * altura);
-    resposta = Number(resposta);
-    let valor_aredondado = resposta.toFixed(2);
-    resultado.innerHTML = valor_aredondado;
     
     
-    let mar = document.getElementById("bloco");
-    mar.style.height = "500px";
-
-    console.log(resposta);
+    if(altura <= 0 && peso <=0 ){
+        
+        let mar = document.getElementById("bloco");
+        mar.style.height = "500px";
+        
+        resultado.innerHTML = "não é possivel.";
+    }
+    
+    else{
+         
+        resposta = peso / (altura * altura);
+        resposta = Number(resposta);
+        
+        let valor_aredondado = resposta.toFixed(2);
+        resultado.innerHTML = valor_aredondado;
+        
+        
+        let mar = document.getElementById("bloco");
+        mar.style.height = "500px";
+        
+        
+        console.log(resposta);
+    }
 }
+
 
